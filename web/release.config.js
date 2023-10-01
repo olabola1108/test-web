c874c9721127550c620b0a0610ba914e826a2718
olabola1108 <olabola1108@gmail.com>
2023-10-01 14:26:30 -0400

    first

---
+++ web/release.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+branches: "main",
+<<<<<<< HEAD
+repositoryUrl: "https://github.com/olabola1108/test-web.git",
+=======
+<<<<<<< HEAD
+repositoryUrl: "https://github.com/olabola1108/test-web.git",
+=======
+repositoryUrl: "https://bitbucket.org/olabola1108/workspace/projects/WEB",
+>>>>>>> ee49cd0 (chore: First release)
+>>>>>>> 3074186 (okay)
+plugins: [
+  '@semantic-release/commit-analyzer',
+  '@semantic-release/release-notes-generator',
+  '@semantic-release/git',
+  '@semantic-release/github']
+ }
