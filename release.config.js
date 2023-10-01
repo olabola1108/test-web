489bea5761b845e458aee702420be9d1a2095990
olabola1108 <olabola1108@gmail.com>
2023-10-01 14:51:10 -0400

    re-add

--- release.config.js
+++ release.config.js
@@ -1,14 +1,6 @@
 module.exports = {
 branches: "main",
-<<<<<<< HEAD
 repositoryUrl: "https://github.com/olabola1108/test-web.git",
-=======
-<<<<<<< HEAD
-repositoryUrl: "https://github.com/olabola1108/test-web.git",
-=======
-repositoryUrl: "https://bitbucket.org/olabola1108/workspace/projects/WEB",
->>>>>>> ee49cd0 (chore: First release)
->>>>>>> 3074186 (okay)
 plugins: [
   '@semantic-release/commit-analyzer',
   '@semantic-release/release-notes-generator',
